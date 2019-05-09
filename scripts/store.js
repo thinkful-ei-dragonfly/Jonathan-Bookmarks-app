9b5457382d21ad079189ae43ebb9cb11fe8bf7bd
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-09 17:23:38 -0500

    Added html

--- scripts/store.js
+++ scripts/store.js
@@ -1,8 +1,1 @@
 'use strict';
-
-const store = {list: [
-  {id:1, title: 'Articles about Baseball', url: 'http://baseballURL.com', rating: 5, description: 'A collection of articles about the game of Baseball', expanded: false},
-  {id:2, title: 'Articles about Basketball', url: 'http://basketballURL.com', rating: 4, description: 'A collection of articles about the game of Basketball', expanded: false},
-  {id:3, title: 'Articles about Football', url: 'http://footballURL.com', rating: 3, description: 'A collection of articles about the game of Football', expanded: false},
-  {id:4, title: 'Articles about Hockey', url: 'http://hockeyURL.com', rating: 2, description: 'A collection of articles about the game of Hockey', expanded: false},
-]};
